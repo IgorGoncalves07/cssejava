d17aa87366243fb66fec7b71bdc44d6e4ab0af16
IgorGoncalves07 <igor.goncalves@aluno.fmpsc.edu.br>
2026-05-14 10:39:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function alternarTexto() {
+
+    let paragrafo = document.getElementById("mensagem");
+
+
+    paragrafo.innerHTML = "O texto foi alterado usando JavaScript";
+}
+
+
+window.alert("Teste")
